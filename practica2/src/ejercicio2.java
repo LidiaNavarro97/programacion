61f888f9c6e671a56bf7e5f64e97e0bab66e8855
LidiaNavarro97 <lnavest97@gmail.com>
2025-10-06 12:20:57 +0200

    Ejercicio 2 acabado

---
+++ practica2/src/ejercicio2.java
@@ -0,0 +1,15 @@
+public class ejercicio2 {
+
+    // Muestra por pantalla todos los elementos de un array de números enteros separados por un espacio.
+}
+public static void main (String[] args){
+    int[] numeros = {1,2,3,4,5};
+
+    System.out.println("Elementos del array: ");
+
+    for(int i = 0; i < numeros.length; i++) {
+        System.out.print(numeros[i]+" ");
+    }
+
+    System.out.println();
+}
