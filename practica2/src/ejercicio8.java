5653d12fc82d986733e67faf9a5b541f0f80852a
LidiaNavarro97 <lnavest97@gmail.com>
2025-10-08 14:02:16 +0200

    creacion ejercicio 8

---
+++ practica2/src/ejercicio8.java
@@ -0,0 +1,16 @@
+
+public class ejercicio8 {
+    import java.util.Scanner;
+}
+        public static void main(String[] args) {
+
+            //Leer la altura de N personas y calcular la altura media.
+            //Calcular cuántas personas tienen una altura superior a la media y cuántas tienen una altura inferior a la media.
+            //El valor de N se pide por teclado y debe ser entero positivo.
+
+
+
+
+
+
+}
