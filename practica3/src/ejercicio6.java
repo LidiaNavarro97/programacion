96fa62ae6c5b9d0660742d7ca1789f833fe67a4a
LidiaNavarro97 <lnavest97@gmail.com>
2025-11-07 09:55:17 +0100

    ejercicio 6 terminado

--- practica3/src/ejercicio6.java
+++ practica3/src/ejercicio6.java
@@ -11,53 +11,88 @@ public class ejercicio6 {
         // primero declaro las variables de filas y columnas y su matriz
         int filas = 4;
         int columnas = 5;
-        int[][] numeros = new int[filas][columnas];
+        int[][] num = new int[filas][columnas];
 
 
-        //una lista con numeros del 20 al 40
-        ArrayList<Integer> lista= new ArrayList<>();
-        for (int i = 20; i <=40 ; i++) {
-            lista.add(i);
+        // Array con números del 20 al 40
+        int[] numeros = new int[21];
+        int indice = 0;
+        for (int i = 20; i <= 40; i++) {
+            numeros[indice++] = i;
         }
 
-        //mezclar los numeros  de la lista manualmente
+        // Mezclar manualmente el array
         Random random = new Random();
-        for (int i = 0; i < lista.size(); i++) {
-           int posAleatorio= random.nextInt(lista.size());
-
-           int aux = lista.get(i);
-           lista.set(i, lista.get(posAleatorio));
-           lista.set(posAleatorio, aux);
-
+        for (int i = 0; i < numeros.length; i++) {
+            int posAleatoria = random.nextInt(numeros.length);
+            int aux = numeros[i];
+            numeros[i] = numeros[posAleatoria];
+            numeros[posAleatoria] = aux;
         }
 
-        //las variables para el maximo y el minimo
+        // Variables para el máximo y mínimo
         int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
         int maxFila = 0, maxCol = 0, minFila = 0, minCol = 0;
 
-        //rellenar la matriz sin repetir
+        // rellenamos la matriz sin repetir valores
         int index = 0;
         for (int i = 0; i < filas; i++) {
             for (int j = 0; j < columnas; j++) {
-                numeros[i][j] = lista.get(index);
-                index++;
-
-            //buscar maximo y minimo
-            if (numeros[i][j] > max) {
-                max = numeros[i][j];
-                maxFila = i;
-                maxCol = j;
-            }
-            if (numeros[i][j] < min) {
-                min = numeros[i][j];
-                minFila = i;
-                minCol = j;
-            }
+                num[i][j] = numeros[index++];
+                // Buscar máximo y mínimo
+                if (num[i][j] > max) {
+                    max = num[i][j];
+                    maxFila = i;
+                    maxCol = j;
+                }
+                if (num[i][j] < min) {
+                    min = num[i][j];
+                    minFila = i;
+                    minCol = j;
+                }
             }
         }
 
+        // Calcular sumas
+        int sumaTotal = 0;
+        int[] sumaFilas = new int[filas];
+        int[] sumaColumnas = new int[columnas];
 
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                sumaFilas[i] += num[i][j];
+                sumaColumnas[j] += num[i][j];
+                sumaTotal += num[i][j];
+            }
+        }
 
+        // Mostrar los resultasdo de la tabla
+        System.out.println("------------------------- TABLA -------------------------");
+        System.out.printf("%-6s", " "); // un espacio para el encabezado de ls fila
+        for (int j = 0; j < columnas; j++) {
+            System.out.printf("%6s", "C" + j);
+        }
+        System.out.printf("%10s\n", "Suma Fila");
+
+        for (int i = 0; i < filas; i++) {
+            System.out.printf("Fila %-2d", i);
+            for (int j = 0; j < columnas; j++) {
+                System.out.printf("%6d", num[i][j]);
+            }
+            System.out.printf("%10d\n", sumaFilas[i]);
+        }
+
+        System.out.printf("%-6s", "Suma");
+        for (int j = 0; j < columnas; j++) {
+            System.out.printf("%6d", sumaColumnas[j]);
+        }
+        System.out.printf("%10d TOTAL", sumaTotal);
+
+        // Mostrar máximo y mínimo
+        System.out.println();
+        System.out.printf("Máximo: %d en [%d][%d]\n", max, maxFila, maxCol);
+        System.out.printf("Mínimo: %d en [%d][%d]\n", min, minFila, minCol);
     }
+}
 
