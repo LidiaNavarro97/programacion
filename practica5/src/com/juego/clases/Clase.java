23d3d96f7c8d211a4ceb4a3388768aa45d37b708
LidiaNavarro97 <lnavest97@gmail.com>
2026-01-13 16:10:19 +0100

    actualizacion

--- practica5/src/com/juego/clases/Clase.java
+++ practica5/src/com/juego/clases/Clase.java
@@ -1,38 +1,4 @@
 package com.juego.clases;
 
-public class Clase {
-    private String nombre;
-    private int bonusFuerza;
-    private int bonusDestreza;
-    private int bonusInteligencia;
-    private int vidaMaxima;
-
-    public Clase(String nombre, int bonusFuerza, int bonusDestreza,
-                 int bonusInteligencia, int vidaMaxima) {
-        this.nombre = nombre;
-        this.bonusFuerza = bonusFuerza;
-        this.bonusDestreza = bonusDestreza;
-        this.bonusInteligencia = bonusInteligencia;
-        this.vidaMaxima = vidaMaxima;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public int getBonusFuerza() {
-        return bonusFuerza;
-    }
-
-    public int getBonusDestreza() {
-        return bonusDestreza;
-    }
-
-    public int getBonusInteligencia() {
-        return bonusInteligencia;
-    }
-
-    public int getVidaMaxima() {
-        return vidaMaxima;
-    }
+public interface Clase {
 }
