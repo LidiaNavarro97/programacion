9863a24a21b1db30f0fa2d6a87c973c503c2a429
LidiaNavarro97 <lnavest97@gmail.com>
2026-01-14 14:12:54 +0100

    Creacion de la clase Datos

--- practica5/src/com/juego/modelo/Datos.java
+++ practica5/src/com/juego/modelo/Datos.java
@@ -1,4 +1,33 @@
 package com.juego.modelo;
 
+//Esta clase es para representar las estadisticas de los personajes
 public class Datos {
+
+    //Atributos del personaje
+    private int fuerza;
+    private int inteligencia;
+    private int destreza;
+    private int vidaMax;
+
+    //CONSTRUCTOR
+    public Datos( int fuerza, int inteligencia, int destreza, int vidaMax) {
+        this.fuerza = fuerza;
+        this.inteligencia = inteligencia;
+        this.destreza = destreza;
+        this.vidaMax = vidaMax;
+    }
+
+    //Aplicar un bonus a las estadisticas
+    public void bonus( int fuerza, int inteligencia, int destreza){
+        this.fuerza += fuerza;
+        this.inteligencia +=  inteligencia;
+        this.destreza += destreza;
+    }
+
+    //Devuelve la vida maxima del personaje
+    public int getVidaMax() {
+        return vidaMax;
+    }
+
+
 }
