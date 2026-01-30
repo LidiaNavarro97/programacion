4ff924ca71c2a8b0f26f98aa2d7b239bb67b820e
LidiaNavarro97 <lnavest97@gmail.com>
2026-01-30 11:47:24 +0100

    cambios realizados en clase

--- practica5/src/com/juego/modelo/Combate.java
+++ practica5/src/com/juego/modelo/Combate.java
@@ -1,13 +1,12 @@
 package com.juego.modelo;
 
 import java.util.Scanner;
-import com.juego.habilidades.*;
+import com.juego.habilidades.Habilidad;
 
 public class Combate {
 
-    Personaje p1;
-    Personaje p2;
-
+    private Personaje p1;
+    private Personaje p2;
     private Scanner sc = new Scanner(System.in);
 
     public Combate(Personaje p1, Personaje p2) {
@@ -15,35 +14,70 @@ public class Combate {
         this.p2 = p2;
     }
 
-    public void combate1() {
-        int contador = 1;
+    public void iniciarCombate() {
 
-        while(p1.getVida()>0 && p2.getVida()>0) {
-            System.out.println("Turno" +contador);
-            System.out.println(p1.getVistaPersonaje());
+        boolean combateActivo = true; // para saber si el combate sigue activo
+        int turno = 1; //contador de turno
+
+        while (combateActivo) {
+
+            System.out.println("TURNO " + turno); //para mostrar el turno
+            System.out.println(p1.getVistaPersonaje()); //para mostrar el estado de los personajes
             System.out.println(p2.getVistaPersonaje());
 
-            //elegir la habilidad
-            this.elegirHabilidad();
+            // turno 1
+            System.out.println("Turno de " + p1.getNombre());
+            Habilidad habP1 = elegirHabilidad(p1);
+            habP1.usar(p2);
 
-            //efecto habilidades
-            this.efectoHabilidades();
-            contador++;
+            // Comprobar si p2 ha muerto
+            if (p2.getVida() <= 0) {
+                System.out.println(p2.getNombre() + "derrotado");
+                combateActivo = false;
+                break;
+            }
+
+            // turno 2
+            System.out.println("Turno de " + p2.getNombre());
+            Habilidad habP2 = elegirHabilidad(p2);
+            habP2.usar(p1);
+
+            // Comprobar si p1 ha muerto
+            if (p1.getVida() <= 0) {
+                System.out.println(p1.getNombre() + " derrotado");
+                combateActivo = false;
+            }
+
+            turno++; //si nadie ha muerto empieza un nuevo turno
         }
 
-        if(p1.getVida()==0) {
-            System.out.println("El ganador es P1");
-        }
-        else {
-            System.out.println("El ganador es P2");
+        // ganador
+        if (p1.getVida() > 0) {
+            System.out.println("El ganador es " + p1.getNombre());
+        } else {
+            System.out.println("El ganador es " + p2.getNombre());
         }
     }
 
-    private void elegirHabilidad() {
+    // Elegir habilidad
+    private Habilidad elegirHabilidad(Personaje p) {
+
+        System.out.println("Elige una habilidad:");
+        Habilidad[] habilidades = p.getHabilidades(); // vemos todas las habilidades del personaje
+
+        for (int i = 0; i < habilidades.length; i++) { //recorro todas las habilidades
+            System.out.println(i + habilidades[i].getNombre()); // muestro todas las habilidades
+        }
+
+        // declaro una variale que le llamamos opcion
+        int opcion;
+        // para ver si la opcion es valida
+        do {
+            opcion = sc.nextInt();
+        } while (opcion < 0 || opcion >= habilidades.length);
+
+        //devolver la habilidad que he elegido
+        return habilidades[opcion];
     }
-
-    private void efectoHabilidades() {
-    }
-
-
 }
+
--- practica5/src/com/juego/modelo/Personaje.java
+++ practica5/src/com/juego/modelo/Personaje.java
@@ -4,6 +4,9 @@ import com.juego.clases.Clase;
 import com.juego.habilidades.Habilidad;
 import com.juego.razas.Raza;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Personaje {
 
     private String nombre;
@@ -11,13 +14,21 @@ public class Personaje {
     private Raza raza;
     private Datos datos;
     private int vidaActual;
+    private List<Habilidad> habilidades;
 
     public Personaje(String nombre, Clase clase, Raza raza) {
         this.nombre = nombre;
         this.clase = clase;
         this.raza = raza;
         this.vidaActual = 100; // por ejemplo
+
+        //cargar las habilidades desde la clase
+        habilidades = new ArrayList<>();
+        for (Habilidad h : clase.getHabilidades()) {
+            habilidades.add(h);
+        }
     }
+
 
     // los get
 
@@ -43,5 +54,12 @@ public class Personaje {
 
     public void setVida(int vidaActual) {
         this.vidaActual = vidaActual;
+    }
+
+    public int[] getHabilidades() {
+        return habilidades;
+    }
+    public void danio(int cantidad) {
+
     }
 }
