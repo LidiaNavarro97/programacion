30c1b7f97969bfa8970180610eaa8928a9927742
LidiaNavarro97 <lnavest97@gmail.com>
2026-02-23 13:34:48 +0100

    cracion practica 7 de nuevo y empiezo con el txt

---
+++ practica7/src/com/rpg/utils/TxtHelper.java
@@ -0,0 +1,39 @@
+package com.rpg.utils;
+import com.rpg.model.Ciudades;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TxtHelper {
+
+    public static List<Ciudades> leerCiudades() {
+
+        List<Ciudades> listaCiudades = new ArrayList<>();
+
+        try {
+            List<String> lineas = Files.readAllLines(
+                    Paths.get("practica7/ficheros/ciudades.txt"));
+
+            for (String linea : lineas) {
+
+                String[] s = linea.split(";");
+
+                Ciudades c = new Ciudades(
+                        s[0],
+                        Integer.parseInt(s[1]),
+                        s[2],
+                        Integer.parseInt(s[3])
+                );
+
+                listaCiudades.add(c);
+            }
+
+        } catch (IOException e) {
+            System.out.println("No se ha podido abrir el fichero.");
+        }
+
+        return listaCiudades;
+    }
+}
