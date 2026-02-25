14b3cf4e770d0419af7a0bc8d94e8197ae4f77b8
LidiaNavarro97 <lnavest97@gmail.com>
2026-02-25 14:26:40 +0100

    Hecha la clase personaje, y comprobacion en el main

--- practica7/src/com/rpg/utils/JsonHelper.java
+++ practica7/src/com/rpg/utils/JsonHelper.java
@@ -3,6 +3,7 @@ package com.rpg.utils;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.rpg.model.Item;
+import com.rpg.model.Personaje;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -30,6 +31,25 @@ public class JsonHelper {
             throw new RuntimeException(e);
         }
         return listaItem;
+    }
+
+    public static List<Personaje> leerPersonaje() {
+
+        List<Personaje> listaPersonaje = new ArrayList<>();
+
+        try {
+            FileReader reader = new FileReader("practica7/ficheros/personajes.json");
+            Gson g = new Gson();
+            List<Personaje> lista = g.fromJson(reader, new TypeToken<List<Personaje>>() {}.getType());
+
+            for (Personaje p : lista){
+                System.out.println(p.getNombre());
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return listaPersonaje;
     }
 }
 
