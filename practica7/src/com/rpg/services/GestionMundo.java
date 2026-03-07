bf3d217325813717cd1c1fa2d3eb873ab4821bde
LidiaNavarro97 <lnavest97@gmail.com>
2026-03-07 12:18:34 +0100

    Carga de datos y creacion del for para el mapa de los items

--- practica7/src/com/rpg/services/GestionMundo.java
+++ practica7/src/com/rpg/services/GestionMundo.java
@@ -3,7 +3,11 @@ package com.rpg.services;
 import com.rpg.model.Ciudades;
 import com.rpg.model.Item;
 import com.rpg.model.Personaje;
+import com.rpg.utils.JsonHelper;
+import com.rpg.utils.TxtHelper;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -15,9 +19,41 @@ public class GestionMundo {
     private List<Personaje> personajes;
     private List<Item> items;
 
-    //mapa para buscar los items por id del objeto Item
+    //hago un map para buscar los items por el id del objeto Item
     private Map<String, Item> mapaItems;
 
+
+    //CONSTRUCTOR
+
+    public GestionMundo() {
+        ciudades = new ArrayList<>();
+        items = new ArrayList<>();
+        personajes = new ArrayList<>();
+        mapaItems = new HashMap<>();
+    }
+
+    //cargo los datos
+
+    public void cargarTodo() {
+
+        System.out.println("Cargando ciudades...");
+        ciudades = TxtHelper.leerCiudades();
+
+        System.out.println("Cargando items...");
+        items = JsonHelper.leerItem();
+
+        System.out.println("Cargando personajes...");
+        personajes = JsonHelper.leerPersonaje();
+
+
+    //hago un foreach para crear un mapa de items, asi guardo la clave id y el valor que seria el objeto Item
+
+        for (Item i : items) {
+            mapaItems.put(i.getId(), i);
+        }
+
+        System.out.println("Items cargados: " + mapaItems.size());
+    }
 
 
 }
