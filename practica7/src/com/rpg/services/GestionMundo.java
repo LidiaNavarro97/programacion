c7c7604e716d07933bad848a40402b9b161132fd
LidiaNavarro97 <lnavest97@gmail.com>
2026-03-07 12:09:28 +0100

    Empiezo la clase GestionMundo para cargar los datos

--- practica7/src/com/rpg/services/GestionMundo.java
+++ practica7/src/com/rpg/services/GestionMundo.java
@@ -1,5 +1,23 @@
 package com.rpg.services;
 
+import com.rpg.model.Ciudades;
+import com.rpg.model.Item;
+import com.rpg.model.Personaje;
+
+import java.util.List;
+import java.util.Map;
+
 public class GestionMundo {
+
+    //listo las clases
+
+    private List<Ciudades> ciudades;
+    private List<Personaje> personajes;
+    private List<Item> items;
+
+    //mapa para buscar los items por id del objeto Item
+    private Map<String, Item> mapaItems;
+
+
 
 }
