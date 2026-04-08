7603dd22c07ed4c13e34d75619f4cd7ed94385b5
Lidia <lnavest97@gmail.com>
2026-04-08 12:39:53 +0200

    Creacion de clases

---
+++ practica8/src/rpg/dao/ConexionDB.java
@@ -0,0 +1,14 @@
+package rpg.dao;
+
+import java.sql.ResultSet;
+
+public class ConexionDB {
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/XRPG";
+    private static final String USER = "xrpg_user";
+    private static final String PASSWORD = "xrpg_password";
+
+
+    public ResultSet conectar(String query) {
+    }
+}
