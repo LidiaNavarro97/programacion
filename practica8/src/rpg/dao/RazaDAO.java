d56ddb47a763c8a17518ad23679486d80f3f5b7e
Lidia <lnavest97@gmail.com>
2026-04-13 13:55:43 +0200

    Clase razaDAO

---
+++ practica8/src/rpg/dao/RazaDAO.java
@@ -0,0 +1,52 @@
+package rpg.dao;
+
+import rpg.model.Raza;
+import java.sql.*; // Importo todo el sql para no liarme
+import java.util.ArrayList;
+import java.util.List;
+
+public class RazaDAO {
+    private List<Raza> razas;
+
+    public RazaDAO(){
+        this.razas = new ArrayList<>();
+    }
+
+    public List<Raza> cargarRazas(){
+        // Limpio la lista por si acaso se llama dos veces al metodo
+        razas.clear();
+
+        String sql = "SELECT * FROM Razas";
+
+        // Uso la conexion de mi clase auxiliar
+        Connection con = ConexionDB.obtenerConexion();
+
+        if (con != null) {
+            try {
+                Statement stmt = con.createStatement();
+                ResultSet rs = stmt.executeQuery(sql);
+
+                while (rs.next()) {
+                    // OJO: Los nombres de las columnas deben ser igual que en el init.sql
+                    // En tu SQL pusiste "bonificador_vida", no "bonificadorVida"
+                    Raza r = new Raza(
+                            rs.getInt("id"),
+                            rs.getString("nombre"),
+                            rs.getInt("bonificador_vida"),
+                            rs.getInt("bonificador_fuerza")
+                    );
+                    razas.add(r);
+                }
+
+                // Cierro
+                rs.close();
+                stmt.close();
+                ConexionDB.cerrarConexion(con);
+
+            } catch (SQLException e) {
+                System.out.println("Error al leer las razas: " + e.getMessage());
+            }
+        }
+        return razas;
+    }
+}
