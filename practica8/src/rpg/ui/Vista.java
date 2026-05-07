96eecf981a1c6fb737cb8c63bf6897beb872db20
LidiaNavarro97 <lnavest97@gmail.com>
2026-05-07 17:57:48 +0200

    Hago los metodos para mostrar los mensajes por pantalla

---
+++ practica8/src/rpg/ui/Vista.java
@@ -0,0 +1,22 @@
+package rpg.ui;
+
+public class Vista {
+
+    public Vista(){}
+
+    public void mostrarMensaje(String mensaje){
+        System.out.println(mensaje);
+    }
+
+    public void mostrarOpciones(){
+        System.out.println(" 1- Crear personaje. ");
+        System.out.println(" 2- Viajar de ciudad. ");
+        System.out.println(" 3- Comprar Items. ");
+        System.out.println(" 4- Cobro de impuestos. ");
+        System.out.println(" 5- Combate. ");
+        System.out.println(" 6- Salir. ");
+
+
+    }
+
+}
